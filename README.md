8a746b6db35a2a5aa3388809279fe0f4e3a958ba
Nick <nicklonz@gmail.com>
2013-08-17 20:45:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# One Month Rails
+
+This is the sample demo application for
+[*One Month Rails: Teach Yourself to Code*] (http://onemonthrails.com)
+
+By NPL - Student in Training 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails::Application.config.secret_key_base = '1f164092bb0ac0cdca2eedb2ddee97b8bda04a6845e732e32aa55c218fe6f9463104e09c295c705cf9e072d94efdb341495111556579cefafde8c417458362d5'
