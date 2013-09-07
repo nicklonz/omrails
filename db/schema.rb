a29b24d3d7e9eb79fa51723836e40f8395158055
Nick <nicklonz@gmail.com>
2013-09-07 10:20:05 -0400

    DB Error Fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906224348) do
+ActiveRecord::Schema.define(version: 20130906233216) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
