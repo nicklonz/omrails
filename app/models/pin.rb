5f469338c95f79bbe3b907aa2c7e96de8f662e3b
Nick <nicklonz@gmail.com>
2013-09-13 17:16:21 -0400

    update pin crisis still trying to destroy

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,10 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
-  attr_accessible :destroy
-  attr_accessible :show
-  attr_accessible :edit
+  # attr_accessible :description
+  # attr_accessible :destroy
+  # attr_accessible :show
+  # attr_accessible :edit
 
+  # validates :user_id, presence: true
+  
   belongs_to :user
-  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+# attr_accessible :email, :password, :password_confirmation, :remember_me, :name
  	
 has_many :pins, :dependent => :destroy
 
