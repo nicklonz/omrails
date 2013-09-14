942c734c0fd3c01200ad1619bfa66715dee19a33
Nick <nicklonz@gmail.com>
2013-09-14 19:42:25 -0400

    Fixed 's' Error

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
 module Omrails
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
